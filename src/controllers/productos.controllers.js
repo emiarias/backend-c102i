75b806ca630d2ca0cc3b8c6428751f0bc4617c65
Emilse Arias <emilsearias@gmail.com>
2024-10-02 09:43:21 -0300

    agregamos una pregunta cuando la base de datos no encuentra el id enviado

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -46,7 +46,9 @@ export const obtenerProducto = async(req, res)=>{
         //pedirle a la BD que busque ese producto que tiene tal id
         const productoBuscado = await Producto.findById(req.params.id);
         //agregar una respuesta adicional cuando productoBuscado es null ---
-        
+        if(!productoBuscado){
+            return res.status(404).json({mensaje: 'El producto no fue encontrado'})
+        }
         //enviar en la respuesta el producto
         console.log(productoBuscado)
         res.status(200).json(productoBuscado)
